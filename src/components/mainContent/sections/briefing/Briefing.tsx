cfb4854c78b687803cff13f7fe1889eceeeed5cd
Alexander Revenko <revenko.a@bk.ru>
2023-11-12 14:23:02 +0700

    Исправил наименования селектора в Briefing.module.scss

--- src/components/mainContent/sections/briefing/Briefing.tsx
+++ src/components/mainContent/sections/briefing/Briefing.tsx
@@ -7,7 +7,7 @@ function Briefing() {
       <div className={style.container__avatar}>
         <img className={style.container__avatar__img} src={avatar} alt="Avatar"/>
       </div>
-      <div className={style.container__container__textContainer}>
+      <div className={style.container__textContainer}>
         <p className='main-text'>Добро пожаловать.</p>
         <p className='main-text'>В меню слева вы увидите мои контакты и стек технологий которым я владею. Контакты и стек (все 3 раздела
           по отдельности) отсортированы по приоритету сверху вниз от большего к меньшему приоритету. </p>
