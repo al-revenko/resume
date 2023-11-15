d3791f240257d6d4cda8fb6e6af81068ff4e3afb
Alexander Revenko <revenko.a@bk.ru>
2023-11-15 12:39:58 +0700

    Исправил написания атрибутов в BurgerBtn

--- src/components/comps/burger-btn/BurgerBtn.tsx
+++ src/components/comps/burger-btn/BurgerBtn.tsx
@@ -4,11 +4,11 @@ const BurgerBtn = () => {
   return (
     <button className={style.container}>
       <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 200 200">
-        <g stroke-width="6.5" stroke-linecap="round">
+        <g strokeWidth="6.5" strokeLinecap="round">
           <path
             d="M72 82.286h28.75"
             fill="#009100"
-            fill-rule="evenodd"
+            fillRule="evenodd"
           />
           <path
             d="M100.75 103.714l72.482-.143c.043 39.398-32.284 71.434-72.16 71.434-39.878 0-72.204-32.036-72.204-71.554"
@@ -17,7 +17,7 @@ const BurgerBtn = () => {
           <path
             d="M72 125.143h28.75"
             fill="#009100"
-            fill-rule="evenodd"
+            fillRule="evenodd"
           />
           <path
             d="M100.75 103.714l-71.908-.143c.026-39.638 32.352-71.674 72.23-71.674 39.876 0 72.203 32.036 72.203 71.554"
@@ -26,12 +26,12 @@ const BurgerBtn = () => {
           <path
             d="M100.75 82.286h28.75"
             fill="#009100"
-            fill-rule="evenodd"
+            fillRule="evenodd"
           />
           <path
             d="M100.75 125.143h28.75"
             fill="#009100"
-            fill-rule="evenodd"
+            fillRule="evenodd"
           />
         </g>
       </svg>
